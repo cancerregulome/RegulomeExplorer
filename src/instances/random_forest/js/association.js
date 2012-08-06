6f347274591cb4d07fe8c09ddcdae3571a991363
Dick Kreisberg <rkreisberg@systemsbiology.org>
2012-08-06 11:28:43 -0700

    -log10(p) for rf instance;

--- src/instances/random_forest/js/association.js
+++ src/instances/random_forest/js/association.js
@@ -2,53 +2,54 @@ if (re.model === undefined) re.model = {};
 
 re.model.association =  {
 	types : [		
-		{ 	id : 'logged_pvalue',
-			label : '-log10(p)',
-			ui : {
-				filter : { 
- 					component: {
-	 							xtype : 'numberfield',
-                                id:'logged_pvalue',
-                                name :'logged_pvalue',
-                                allowNegative: false,
-                                decimalPrecision : 8,
-                                emptyText : 'Input value...',
-                                invalidText:'This value is not valid.',
-                                maxValue:0.9,
-                                minValue:0,
-                                tabIndex : 1,
-                                validateOnBlur : true,
-                                fieldLabel : '-log10(p) <=',
-                                defaultValue : 0.5,
-                                value : 0.5
-                            }
-				},
-					grid : {
-						column : { header : "logged_pvalue", width : 50 , id: 'logged_pvalue' , dataIndex : 'logged_pvalue', hidden: true},
-						store_index : 'logged_pvalue'
-					}
-			},
-			query : {
-				id : 'logged_pvalue',
-				clause : 'logged_pvalue <= ',
-				order_direction : 'DESC'
-			},
-			vis : {
-				network : {
-					edgeSchema : {name: "logged_pvalue", type: "number" }
-				},
-				tooltip : {
-					entry : { pvalue : 'logged_pvalue' }
-				},
-                scatterplot : {
-                    values : {
-                                               min : 0,
-                                                max : 0.5
-                                            },
-                    color_scale : pv.Scale.linear(0.5,0).range('blue','red')
+	{   id : 'logged_pvalue',
+            label : '-log10(p)',
+            ui : {
+                filter : {
+                    component: {
+                        xtype : 'numberfield',
+                        id:'logged_pvalue',
+                        name :'logged_pvalue',
+                        allowNegative: true,
+                        decimalPrecision : 0,
+                        emptyText : 'Input value...',
+                        invalidText:'This value is not valid.',
+                        maxValue:300,
+                        minValue:0.0,
+                        tabIndex : 1,
+                        validateOnBlur : true,
+                        fieldLabel : '-log10(p) >=',
+                        defaultValue: 6,
+                        value : 6
+                    }
+                },
+                grid : {
+                    column : { header : "-log10(p)", width : 50 , id: 'logged_pvalue' , dataIndex : 'logged_pvalue', hidden: false},
+                    store_index : 'logged_pvalue'
                 }
-			}
-		},
+            },
+            query : {
+                id : 'logged_pvalue',
+                clause : 'logged_pvalue >= ',
+                order_direction : 'DESC'
+            },
+            vis : {
+                network : {
+                    edgeSchema : {name: "logged_pvalue", type: "number" }
+                },
+                tooltip : {
+                    entry : { '-log10(p)' : 'logged_pvalue' }
+                },
+                scatterplot: {
+                    values: {
+                        min:0,
+                        floor : 0,
+                        ceil: 300
+                    },
+                    color_scale : pv.Scale.linear(0,50).range('blue','red')
+                }
+            }
+        },
 		{ 	id : 'correlation',
 			label : 'Correlation',
 			ui : {
